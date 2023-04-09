7a83d4622ffbe5a4b6f035e15af3081904e7d87c
zhirayrshirinyan <zhirayr.shirinyan@wgm.com>
2023-04-09 23:47:47 +0400

    Refactor: initial Vite code

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../public/vite.svg'
+import globeLogo from './assets/globe.svg'
 import './App.css'
+import Table from './components/Table'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={globeLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Countries table</h1>
       <div className="card">
-        <button type="button" onClick={() => setCount((cnt) => cnt + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <Table />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
---
+++ src/components/Table/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.scss'
+
+const Table = () => {
+  return (
+    <div className="container">
+      <p>Table should be here</p>
+    </div>
+  )
+}
+
+export default Table
