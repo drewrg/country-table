72b3a2ae38a87e6e3b9bf66a517b99250c1068ae
zhirayrshirinyan <zhirayr.shirinyan@wgm.com>
2023-04-10 02:20:52 +0400

    Add: initial table component structure

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,11 +1,99 @@
 import React from 'react'
+import { useTable } from 'react-table'
 import './index.scss'
 
 const Table = () => {
+  const data: Array<any> = React.useMemo(
+    () => [
+      {
+        col1: 'Hello',
+        col2: 'World',
+      },
+      {
+        col1: 'react-table',
+        col2: 'rocks',
+      },
+      {
+        col1: 'whatever',
+        col2: 'you want',
+      },
+    ],
+    [],
+  )
+
+  const columns: Array<any> = React.useMemo(
+    () => [
+      {
+        Header: 'Column 1',
+        accessor: 'col1',
+      },
+      {
+        Header: 'Column 2',
+        accessor: 'col2',
+      },
+    ],
+    [],
+  )
+
+  const tableInstance = useTable({ columns, data })
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance
+
   return (
-    <div className="container">
-      <p>Table should be here</p>
-    </div>
+    // apply the table props
+    <table {...getTableProps()}>
+      <thead>
+        {
+          // Loop over the header rows
+          headerGroups.map((headerGroup) => (
+            // Apply the header row props
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {
+                // Loop over the headers in each row
+                headerGroup.headers.map((column) => (
+                  // Apply the header cell props
+                  <th {...column.getHeaderProps()}>
+                    {
+                      // Render the header
+                      column.render('Header')
+                    }
+                  </th>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </thead>
+      {/* Apply the table body props */}
+      <tbody {...getTableBodyProps()}>
+        {
+          // Loop over the table rows
+          rows.map((row) => {
+            // Prepare the row for display
+            prepareRow(row)
+            return (
+              // Apply the row props
+              <tr {...row.getRowProps()}>
+                {
+                  // Loop over the rows cells
+                  row.cells.map((cell) => {
+                    // Apply the cell props
+                    return (
+                      <td {...cell.getCellProps()}>
+                        {
+                          // Render the cell contents
+                          cell.render('Cell')
+                        }
+                      </td>
+                    )
+                  })
+                }
+              </tr>
+            )
+          })
+        }
+      </tbody>
+    </table>
   )
 }
 
