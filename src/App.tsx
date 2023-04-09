df3d9f71b882898360f02ed09fd007dd43cda919
zhirayrshirinyan <zhirayr.shirinyan@wgm.com>
2023-04-09 22:14:23 +0400

    Add: Prettier and Husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../public/vite.svg';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '../public/vite.svg'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
@@ -19,23 +19,17 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button type="button" onClick={() => setCount((cnt) => cnt + 1)}>
-          count is
-          {' '}
-          {count}
+          count is {count}
         </button>
         <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
